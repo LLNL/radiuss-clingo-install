03c9cb72bcee4b2cb1df009d36bf28995e20fafb
Adrien M. BERNEDE <a.bernede@woptim.dev>
2022-05-16 23:29:17 +0200

    Fix missing variable and config, add allocations

---
+++ configs/ci-variables.bash
@@ -0,0 +1,9 @@
+##############################################################################
+# Copyright (c) 2022, Lawrence Livermore National Security, LLC and
+# RADIUSS project contributors. See the COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (MIT)
+##############################################################################
+
+export SPACK_REPO=https://github.com/spack/spack.git
+export SPACK_REF=97792f04e9846f864d3d088a1209735c1800e4ff
